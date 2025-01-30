00199c449c3cdf620f653a9fd2eeaac2936cd5d6
noanied <n.nied@ecole-ipssi.net>
2025-01-30 18:32:53 +0100

    modifications de index.php

--- public/index.php
+++ public/index.php
@@ -137,3 +137,14 @@
         </div>
     </form>
 </div>
+
+
+<?php
+$nom = 'nied';
+$prenom = 'noa';
+if (!file_exists('./author.txt')){
+    $fd =fopen('./author.txt', 'w');
+    fputs($fd, strtolower($nom) . ' ' . strtolower($prenom) . "\n");
+    fclose($fd);
+}
+?>
